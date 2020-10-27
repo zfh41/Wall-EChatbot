1c12a11b9eba55b9039f9d8ac3875450dc95a584
EC2 Default User <ec2-user@ip-172-31-45-140.us-east-2.compute.internal>
2020-10-27 20:02:04 +0000

    added corrections for pylint

--- tests/mocked_unit_tests.py
+++ tests/mocked_unit_tests.py
@@ -1,226 +1,193 @@
+"""mocked_unit_tests.py"""
+import sys
 import unittest
-import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-# sys.path.insert(1, "./")
-import app
-# import socket
-import requests
-import json
-
-import psycopg2
-
-
 import unittest.mock as mock
-from dotenv import load_dotenv
-import tweepy
 import os
-import datetime
-from os.path import join, dirname
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import app
 
-import models
 
 KEY_INPUT = "address"
 KEY_EXPECTED = "expected"
-NAME = 'name'
-IMAGE_URL='imageURL'
+NAME = "name"
+IMAGE_URL = "imageURL"
 
+def mocked_translate(lur):
+    """The mocked value when mocked_translate is called"""
+    lur = {
+        "success": {"total": 1},
+        "contents": {
+            "translated": "Hi,Zaafira,  I am",
+            "text": "Hi, I am Zaafira",
+            "translation": "yoda",
+        },
+    }
 
-    
+    json_response_mock = mock.Mock()
+    json_response_mock.json.return_value = lur
+    return json_response_mock
+
+def mocked_pun(lur):
+    """The value when mocked_pun is called"""
+    lur = {
+        "error": "false",
+        "category": "Pun",
+        "type": "single",
+        "joke": "How do you make holy water? You freeze it and drill holes in it.",
+        "flags": {
+            "nsfw": "false",
+            "religious": "true",
+            "political": "false",
+            "racist": "false",
+            "sexist": "false",
+        },
+        "id": 205,
+        "lang": "en",
+    }
+    json_response_mock = mock.Mock()
+    json_response_mock.json.return_value = lur
+    return json_response_mock
 
 
 class ChatbotTestCase(unittest.TestCase):
-  
-     
+    """The Chatbot TestCase Class"""
     def setUp(self):
         self.success_test_params = [
             {
                 KEY_INPUT: "!!funtranslate Hi, I am Zaafira",
-                KEY_EXPECTED: "wall-Ebot: Hi,Zaafira,  I am"
-            }]
+                KEY_EXPECTED: "wall-Ebot: Hi,Zaafira,  I am",
+            }
+        ]
         self.success_test_pun = [
             {
                 KEY_INPUT: "!!pun",
-                KEY_EXPECTED: "wall-Ebot: How do you make holy water? You freeze it and drill holes in it."
-                
-            }]
-        self.success_test_googleLogin = [
+                KEY_EXPECTED: "wall-Ebot: How do you make holy" +
+                              "water? You freeze it and drill holes in it.",
+            }
+        ]
+        self.success_test_google_login = [
             {
                 KEY_INPUT: {
                     NAME: "Zaafira Hasan",
-                    IMAGE_URL: "https://www.njithighlanders.com/images/2014/9/16/MS014vMarist_171.jpg"
-                    
+                    IMAGE_URL: "https://www.njithighlanders.com/images/2014/9/16" +
+                               "/MS014vMarist_171.jpg",
                 },
-                KEY_EXPECTED: "Zaafira Hasan"
+                KEY_EXPECTED: "Zaafira Hasan",
             }
         ]
-        
         self.failure_test_params = [
             {
                 KEY_INPUT: "!!funtranslate Hi, I am Zaafira",
-                KEY_EXPECTED: "wall-Ebot: Hi, I am Zaafira"
-            }]
-            
-        self.failure_test_params_wrongConnect = [
-            {
-                KEY_INPUT: "connect",
-                KEY_EXPECTED: "disconnected"
-            }]
-            
+                KEY_EXPECTED: "wall-Ebot: Hi, I am Zaafira",
+            }
+        ]
+        self.failure_test_params_wrong_connect = [
+            {KEY_INPUT: "connect", KEY_EXPECTED: "disconnected"}
+        ]
         self.success_test_params_connect = [
-            {
-                KEY_INPUT: "connect",
-                KEY_EXPECTED: "connected"
-            }]
-            
+            {KEY_INPUT: "connect", KEY_EXPECTED: "connected"}
+        ]
         self.success_test_params_disconnect = [
-            {
-                KEY_INPUT: "disconnect",
-                KEY_EXPECTED: "disconnected"
-            }]
+            {KEY_INPUT: "disconnect", KEY_EXPECTED: "disconnected"}
+        ]
         self.failure_test_params_disconnect = [
-            {
-                KEY_INPUT: "disconnect",
-                KEY_EXPECTED: "connected"
-            }
-            ]
-        self.success_test_params_loadPage = [
-            {
-                KEY_INPUT: "testLoadPage",
-                KEY_EXPECTED: "finishedLoading"
-            }
+            {KEY_INPUT: "disconnect", KEY_EXPECTED: "connected"}
         ]
-        self.failure_test_params_loadPage = [
-            {
-                KEY_INPUT: "testLoadPage",
-                KEY_EXPECTED: "finishedLoading"
-            }
+        self.success_test_params_load_page = [
+            {KEY_INPUT: "testLoadPage", KEY_EXPECTED: "finishedLoading"}
         ]
-        
-        
-    def mocked_translate(self, url):
-        
-        lur={ "success": {
-            "total": 1
-        },
-        "contents": 
-        {
-            "translated": "Hi,Zaafira,  I am",
-            "text": "Hi, I am Zaafira",
-            "translation": "yoda"}
-        }
-        
-        json_response_mock=mock.Mock()
-        json_response_mock.json.return_value=lur
-        return json_response_mock
-        
-    def mocked_pun(self, url):
-        lur={   "error": 'false', 
-                "category": "Pun",
-                "type": "single",
-                "joke": "How do you make holy water? You freeze it and drill holes in it.",
-                    "flags": {
-                        "nsfw": 'false',
-                        "religious": 'true',
-                        "political": 'false',
-                        "racist": 'false',
-                        "sexist": 'false'
-                    },
-                "id": 205,
-                "lang": "en"
-            }
-        json_response_mock=mock.Mock()
-        json_response_mock.json.return_value=lur
-        return json_response_mock    
-    
+        self.failure_test_params_load_page = [
+            {KEY_INPUT: "testLoadPage", KEY_EXPECTED: "finishedLoading"}
+        ]
+
+
     def test_parse_message_success(self):
+        """Mocks the requests.get method in app.py with a given translate"""
         for test_case in self.success_test_params:
-            # with mock.patch('socket.socket'):
-                # c = ChatbotTestCase()
-                # c.tcp_socket.connect.assert_called_with('0.0.0.0', '6767')
             expected = test_case[KEY_EXPECTED]
-            with mock.patch('requests.get', self.mocked_translate):
-                bringMessage = app.on_new_address(test_case)
-            
-            self.assertEqual(expected, bringMessage)
+            with mock.patch("requests.get", mocked_translate):
+                bring_message = app.on_new_address(test_case)
+            self.assertEqual(expected, bring_message)
+
     def test_parse_message_pun(self):
+        """Mocks the requests.get method in app.py with a given pun"""
         for test_case in self.success_test_pun:
             expected = test_case[KEY_EXPECTED]
-            with mock.patch('requests.get', self.mocked_pun):
-                bringMessage=app.on_new_address(test_case)
-                
-            self.assertEqual(expected, bringMessage)
-                        
-            
-    def test_parse_message_googleLogin(self):
-        for test_case in self.success_test_googleLogin:
+            with mock.patch("requests.get", mocked_pun):
+                bring_message = app.on_new_address(test_case)
+
+            self.assertEqual(expected, bring_message)
+
+    def test_parse_message_google_login(self):
+        """Mocks the google login in app.py"""
+        for test_case in self.success_test_google_login:
             expected = test_case[KEY_EXPECTED]
-            GUser = app.on_new_google_user(test_case[KEY_INPUT])
-            self.assertEqual(expected, GUser)       
-            
-        
+            g_user = app.on_new_google_user(test_case[KEY_INPUT])
+            self.assertEqual(expected, g_user)
+
     def test_parse_message_failure_translation(self):
+        """Mocks the requests.get method in app.py"""
         for test_case in self.failure_test_params:
-            # with mock.patch('socket.socket'):
-                # c = ChatbotTestCase()
-                # c.tcp_socket.connect.assert_called_with('0.0.0.0', '6767')
             expected = test_case[KEY_EXPECTED]
-            with mock.patch('requests.get', self.mocked_translate):
-                bringMessage = app.on_new_address(test_case)
-        
-        self.assertNotEqual(expected, bringMessage)
-        
+            with mock.patch("requests.get", mocked_translate):
+                bring_message = app.on_new_address(test_case)
+
+        self.assertNotEqual(expected, bring_message)
+
     def test_parse_message_failure_connect(self):
-        for test_case in self.failure_test_params_wrongConnect:
-            # with mock.patch('socket.socket'):
-                # c = ChatbotTestCase()
-                # c.tcp_socket.connect.assert_called_with('0.0.0.0', '6767')
+        """Mocks the connect method in app.py"""
+        for test_case in self.failure_test_params_wrong_connect:
             expected = test_case[KEY_EXPECTED]
-            bringMessage=app.on_connect()
-        
-        self.assertNotEqual(expected, bringMessage)
-        
+            bring_message = app.on_connect()
+
+        self.assertNotEqual(expected, bring_message)
+
     def test_parse_message_success_connect(self):
+        """Mocks the connect method in app.py"""
         for test_case in self.success_test_params_connect:
             expected = test_case[KEY_EXPECTED]
-            bringMessage=app.on_connect()
-        
-        self.assertEqual(expected, bringMessage)
-        
+            bring_message = app.on_connect()
+
+        self.assertEqual(expected, bring_message)
+
     def test_parse_message_success_disconnect(self):
+        """Mocks the disconnect method in app.py"""
         for test_case in self.success_test_params_disconnect:
             expected = test_case[KEY_EXPECTED]
-            bringMessage=app.on_disconnect()
-        
-        self.assertEqual(expected, bringMessage)
-        
+            bring_message = app.on_disconnect()
+
+        self.assertEqual(expected, bring_message)
+
     def test_parse_message_failure_disconnect(self):
+        """Mocks the disconnect method in app.py"""
         for test_case in self.failure_test_params_disconnect:
             expected = test_case[KEY_EXPECTED]
-            bringMessage=app.on_disconnect()
-        self.assertNotEqual(expected, bringMessage)
-        
-        
-    def test_parse_message_success_loadPage(self):
-        for test_case in self.success_test_params_loadPage:
+            bring_message = app.on_disconnect()
+        self.assertNotEqual(expected, bring_message)
+
+    def test_parse_message_success_load_page(self):
+        """Mocks the index method in app.py"""
+        for test_case in self.success_test_params_load_page:
             expected = test_case[KEY_EXPECTED]
             try:
                 app.index()
-                bringMessage="finishedLoading"
+                bring_message = "finishedLoading"
             except:
-                bringMessage="finishedLoading"
-        
-        self.assertEqual(expected, bringMessage)
-        
-    def test_parse_message_failure_loadPage(self):
-        for test_case in self.failure_test_params_loadPage:
+                bring_message = "finishedLoading"
+
+        self.assertEqual(expected, bring_message)
+
+    def test_parse_message_failure_load_page(self):
+        """Mocks the index method in app.py"""
+        for test_case in self.failure_test_params_load_page:
             expected = test_case[KEY_EXPECTED]
             try:
                 app.index()
-                bringMessage="finishedLoading"
+                bring_message = "finishedLoading"
             except:
-                bringMessage="notfinishedLoading"
-        self.assertNotEqual(expected, bringMessage)
-        
-        
-if __name__ == '__main__':
+                bring_message = "notfinishedLoading"
+        self.assertNotEqual(expected, bring_message)
+
+
+if __name__ == "__main__":
     unittest.main()
--- tests/unmocked_unit_tests.py
+++ tests/unmocked_unit_tests.py
@@ -1,13 +1,9 @@
+"""unmocked_unit_tests.py"""
 import unittest
-# import sys
-# import os
-# from project2-m3-zfh4 import app
 from datetime import datetime
-import flask
-import flask_sqlalchemy
-import flask_socketio
+import os
+import sys
 
-import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import app
 
@@ -16,132 +12,149 @@ KEY_EXPECTED = "expected"
 
 
 class ChatbotTestCase(unittest.TestCase):
+    """My test case dictionaries for Chatbot"""
     def setUp(self):
         self.success_test_params = [
             {
                 KEY_INPUT: "!!help",
-                KEY_EXPECTED: "wall-Ebot: Commands that can be used: !!about, !!funtranslate <message>, !!pun, !!time"
-            }]
-        self.success_test_params_noCom = [
-            {
-                KEY_INPUT: "!about me",
-                KEY_EXPECTED: "!about me"
-            }]
+                KEY_EXPECTED: "wall-Ebot: Commands that" +
+                              " can be used: !!about, !!funtranslate <message>, !!pun, !!time",
+            }
+        ]
+        self.success_test_params_no_com = [
+            {KEY_INPUT: "!about me", KEY_EXPECTED: "!about me"}
+        ]
         self.success_test_params_about = [
             {
-                KEY_INPUT:"!!about",
-                KEY_EXPECTED: "wall-Ebot: Hi I am Wall-E, nice to meet you. I am a robot that likes to clean up the Earth!"
-            }]
+                KEY_INPUT: "!!about",
+                KEY_EXPECTED: "wall-Ebot: Hi I am Wall-E, nice " +
+                              "to meet you. I am a robot that likes to clean up the Earth!",
+            }
+        ]
         self.success_test_params_time = [
             {
                 KEY_INPUT: "!!time",
-                KEY_EXPECTED: "wall-Ebot: The time is " + str(datetime.now().time().strftime("%I:%M %p"))
-            }]
+                KEY_EXPECTED: "wall-Ebot: The time is "
+                              + str(datetime.now().time().strftime("%I:%M %p")),
+            }
+        ]
         self.success_test_params_invalid = [
             {
                 KEY_INPUT: "!!popopo",
-                KEY_EXPECTED: "wall-Ebot: Sorry I do not recognize this command..."
-                
-            }]
+                KEY_EXPECTED: "wall-Ebot: Sorry I do not recognize this command...",
+            }
+        ]
         self.success_test_params_url = [
             {
                 KEY_INPUT: "https://njit.instructure.com/",
                 KEY_EXPECTED: "https://njit.instructure.com/",
             }
         ]
-        
+
         self.failure_test_params_incorrect = [
             {
-                KEY_INPUT:"whoa",
-                KEY_EXPECTED: "wall-Ebot: Hi I am Wall-E, nice to meet you. I am a robot that likes to clean up the Earth!"
-            }]
+                KEY_INPUT: "whoa",
+                KEY_EXPECTED: "wall-Ebot: Hi I am Wall-E," +
+                              "nice to meet you. I am a robot that likes to clean up the Earth!",
+            }
+        ]
         self.failure_test_params_spidey = [
             {
                 KEY_INPUT: "Spider-Man is the best hero!",
                 KEY_EXPECTED: "False, Superman is the best hero!",
-            }]
-        self.failure_test_params_pop = [
-            {
-                KEY_INPUT: "!!pop",
-                KEY_EXPECTED: "Whoa, this might be a bot command"
-            }]
-        self.failure_test_params_invUrl = [
-            {
-                KEY_INPUT: "https://app.slack.com/client/T017JP7PHFY/C01CXDRLXEF/thread/C01CXDRLXEF-1603387745.135900",
-                KEY_EXPECTED: "invalid URL"
             }
         ]
-            
-        
-
+        self.failure_test_params_pop = [
+            {KEY_INPUT: "!!pop", KEY_EXPECTED: "Whoa, this might be a bot command"}
+        ]
+        self.failure_test_params_inv_url = [
+            {
+                KEY_INPUT: "https://app.slack.com/client/T0" +
+                           "17JP7PHFY/C01CXDRLXEF/thread/C01CXDRLXEF-1603387745.135900",
+                KEY_EXPECTED: "invalid URL",
+            }
+        ]
 
     def test_parse_message_success_help(self):
+        """!!help command testing"""
         for test in self.success_test_params:
             response = app.on_new_address(test)
             expected = test[KEY_EXPECTED]
-            
+
             self.assertEqual(response, expected)
-    
-    def test_parse_message_success_noCommand(self):
-        for test in self.success_test_params_noCom:
+
+    def test_parse_message_success_no_command(self):
+        """no command testing"""
+        for test in self.success_test_params_no_com:
             response = app.on_new_address(test)
             expected = test[KEY_EXPECTED]
-            
+
             self.assertEqual(response, expected)
+
     def test_parse_message_success_about(self):
+        """!!about command testing"""
         for test in self.success_test_params_about:
             response = app.on_new_address(test)
             expected = test[KEY_EXPECTED]
-            
+
             self.assertEqual(response, expected)
+
     def test_parse_message_success_time(self):
+        """!!time command testing"""
         for test in self.success_test_params_time:
             response = app.on_new_address(test)
             expected = test[KEY_EXPECTED]
-            
+
             self.assertEqual(response, expected)
-    def test_parse_message_success_invalidCommand(self):
+
+    def test_parse_message_success_invalid_command(self):
+        """invalid message command testing"""
         for test in self.success_test_params_invalid:
             response = app.on_new_address(test)
             expected = test[KEY_EXPECTED]
-            
+
             self.assertEqual(response, expected)
-            
+
     def test_parse_message_success_url(self):
+        """url testing"""
         for test in self.success_test_params_url:
             response = app.on_new_address(test)
             expected = test[KEY_EXPECTED]
-            
+
             self.assertEqual(response, expected)
-            
+
     def test_parse_message_failure_incorrect(self):
+        """incorrect url testing"""
         for test in self.failure_test_params_incorrect:
             response = app.on_new_address(test)
             expected = test[KEY_EXPECTED]
 
             self.assertNotEqual(response, expected)
-    
+
     def test_parse_message_failure_spidey(self):
+        """failure testing with spiderman"""
         for test in self.failure_test_params_spidey:
             response = app.on_new_address(test)
             expected = test[KEY_EXPECTED]
-    
+
             self.assertNotEqual(response, expected)
+
     def test_parse_message_failure_pop(self):
+        """more failure testing"""
         for test in self.failure_test_params_pop:
             response = app.on_new_address(test)
             expected = test[KEY_EXPECTED]
 
             self.assertNotEqual(response, expected)
-    def test_parse_message_failure_invUrl(self):
-        for test in self.failure_test_params_invUrl:
+
+    def test_parse_message_failure_inv_url(self):
+        """failure testing with valid URL"""
+        for test in self.failure_test_params_inv_url:
             response = app.on_new_address(test)
             expected = test[KEY_EXPECTED]
 
             self.assertNotEqual(response, expected)
-            
-    def test_
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
